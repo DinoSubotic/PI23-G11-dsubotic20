5dcbca567e186dc0a32dafcabc393bf87f8f395c
DinoSubotic <dsubotic20@foi.hr>
2023-04-25 17:49:10 +0200

    Dorađena forma za login

---
+++ EvaluationManagerG11/frmLogin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EvaluationManagerG11 {
+    public partial class frmLogin : Form {
+        public frmLogin() {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e) {
+
+        }
+
+        private void textUserName_TextChanged(object sender, EventArgs e) {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e) {
+
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e) {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+
+        }
+    }
+}
